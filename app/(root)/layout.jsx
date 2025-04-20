6cfe3426ff6afc845d89b9b992150b3d11512731
Abhishek <abhishekmandavi8805@gmail.com>
2025-04-20 22:45:35 +0530

    commit

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const Layout = async ({ children }) => {
     return(
-        <div>
-            {/* <nav>
+        <div className='root-layout'>
+            <nav>
                 <Link href="/" className="flex items-center gap-2">
-                 <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32}/>
+                 <Image src="/logo.svg" alt="Logo" width={32} height={32}/>
                  <h2 className="text-primary-100">InterviewPrep</h2>
                 </Link>
-            </nav> */}
+            </nav> 
             {children}
         </div>
     )
--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+      <section className="flex flex-col sm:flex-row items-center justify-between gap-10 p-6 bg-white rounded-2xl shadow-lg card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2 className="text-2xl font-bold">
+           Ace Your Next Interview with AI-Powered Practice  
+          </h2>
+          <p className="text-lg">
+           Get confident with real questions and actionable feedback 
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image src="/robot.png" alt='robo' width={400} height={400} className="max-sm:hidden"/>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take Interviews</h2>
+      </section>
+
+      <div className="interviews-section">
+       <p>There are no interviews available</p>
+      </div>
+    </>
   )
 }
 
