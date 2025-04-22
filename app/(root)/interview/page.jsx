f7791b6bf7ab048f1d99a71c99f52128f875a32f
Abhishek <abhishekmandavi8805@gmail.com>
2025-04-22 23:36:10 +0530

    commit

---
+++ app/(root)/interview/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Agent from '@/components/Agent'
+import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const page = async () => {
+  const user = await getCurrentUser();
+  return (
+    <>
+     <h3>Interview</h3>
+     <Agent
+      userName={user?.name || ""}
+      userId={user?.id}
+      profileImage={user?.profileURL}
+      type="generate"
+     />
+    </>
+  )
+}
+
+export default page
---
+++ components/Agent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Agent = () => {
+  return (
+    <div className='call-view'>
+        <div className='card-interviewer'>
+            <div className='avatar'>
+                <Image
+                 src="/ai-avatar.png"
+                 alt="profile-image"
+                 width={65}
+                 height={54}
+                 className="object-cover"
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Agent
